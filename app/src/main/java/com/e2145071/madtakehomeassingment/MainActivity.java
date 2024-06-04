1034538dd98228dcf57c8173de06c9e3d2d84211
ekalumin <ekalumin@gmail.com>
2024-06-04 14:16:32 +0530

    Test

--- app/src/main/java/com/e2145071/madtakehomeassingment/MainActivity.java
+++ app/src/main/java/com/e2145071/madtakehomeassingment/MainActivity.java
@@ -2,15 +2,89 @@ package com.e2145071.madtakehomeassingment;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText txtCity;
+    TextView result;
+    private final String  url = "http://api.openweathermap.org/data/2.5/weather";
+    private final String appid = "2993e8025b7403d13cf42b6a8da14d5c";
+    DecimalFormat df = new DecimalFormat("#.##");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        txtCity = findViewById(R.id.txtCity);
+        result = findViewById(R.id.result);
 
+    }
+
+    public void getWeatherDetails(View view) {
+        String tempURL = "";
+        String city = txtCity.getText().toString().trim();
+        if (city.equals("")){
+            result.setText("City field can not be empty.");
+        }else{
+            tempURL = url + "?q=" + city + "&appid=" + appid;
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, tempURL, response -> {
+                Log.d("response", response);
+                String output = "";
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    JSONArray jsonArray = jsonResponse.getJSONArray("weather");
+                    JSONObject jsonObjectWeather = jsonArray.getJSONObject(0);
+                    String description = jsonObjectWeather.getString("description");
+                    JSONObject jsonObjectMain = jsonResponse.getJSONObject("main");
+                    double temp = jsonObjectMain.getDouble("temp") - 273.15;
+                    double feelsLike = jsonObjectMain.getDouble("feels_like") - 273.15;
+                    float pressure = jsonObjectMain.getInt("pressure");
+                    float humidity = jsonObjectMain.getInt("humidity");
+                    JSONObject jsonObjectWind = jsonResponse.getJSONObject("wind");
+                    String wind = jsonObjectWind.getString("speed");
+                    JSONObject jsonObjectClouds = jsonResponse.getJSONObject("clouds");
+                    String clouds = jsonObjectClouds.getString("all");
+                    JSONObject jsonObjectSys = jsonResponse.getJSONObject("sys");
+                    String countryName = jsonObjectSys.getString("country");
+                    String cityName =  jsonResponse.getString("name");
+                    result.setTextColor(Color.rgb(68, 134, 199));
+                    output += "Current weather of " + cityName + " (" + countryName + ")"
+                            + "\n Temp: " + df.format(temp) + " °C"
+                            + "\n Feels Like: " + df.format(feelsLike) + " °C"
+                            + "\n Humidity: " + humidity + "%"
+                            + "\n Description: " + description
+                            + "\n Wind Speed: " + wind + "m/s (meters per second)"
+                            + "\n Cloudiness: " + clouds + "%"
+                            + "\n Pressure: " + pressure + " hPa";
+                    result.setText(output);
+
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }, error -> {
+                Toast.makeText(getApplicationContext(), error.toString().trim(), Toast.LENGTH_SHORT).show();
+            });
+            RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(stringRequest);
+        }
     }
 }
